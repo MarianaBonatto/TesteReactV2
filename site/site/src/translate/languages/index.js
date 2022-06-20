9700a98e4c73a7407fbfb513dbf829bbc0aa0fb0
Thiago Queiroz <thiago.c.queiroz@outlook.com>
2022-06-20 02:27:00 -0300

    correção final de pacotes

--- site/site/src/translate/languages/index.js
+++
@@ -1,9 +0,0 @@
-import { messages as ptMessages } from './pt';
-import { messages as enMessages } from './en';
-
-const messages = {
-    ...ptMessages,
-    ...enMessages
-}
-
-export { messages };
